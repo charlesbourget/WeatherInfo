65c59b60865df0e594a6e73c806e8c23edce820b
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2021-05-06 20:15:59 -0400

    Handling of bad api key and better error messages to user

--- WeatherInfo/MenuBar.swift
+++ WeatherInfo/MenuBar.swift
@@ -34,19 +34,29 @@ class MenuBar {
 
         let task = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
             if let error = error {
-                print("Error with fetching weather from API: \(error)")
+                DispatchQueue.main.async {
+                    self.alertDialog(alertText: "Error with fetching weather from API: \(error)")
+                }
                 self.refreshWeatherData()
                 return
             }
 
             if let httpResponse = response as? HTTPURLResponse, !(200 ... 299).contains(httpResponse.statusCode) {
-                if httpResponse.statusCode == 404 {
+                switch httpResponse.statusCode {
+                case 404:
                     DispatchQueue.main.async {
                         self.alertDialog(alertText: "City not found")
                     }
-                } else {
-                    print("Error with the response, unexpected status code: \(response!)")
+                case 401:
+                    DispatchQueue.main.async {
+                        self.alertDialog(alertText: "Authentification problems. API Key is not valid.")
+                    }
+                default:
+                    DispatchQueue.main.async {
+                        self.alertDialog(alertText: "Error with the response, unexpected status code: \(response!)")
+                    }
                 }
+
                 return
             }
 
